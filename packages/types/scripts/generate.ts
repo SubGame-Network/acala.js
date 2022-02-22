59ec3648c8a0a48c0ea1dd4d5a9cc4f51875721a
JY <y920075@gmail.com>
2022-02-22 15:56:31 +0800

    fix import bug

--- packages/types/scripts/generate.ts
+++ packages/types/scripts/generate.ts
@@ -71,7 +71,6 @@ fs.writeFileSync(
   `// Auto-generated , do not edit
 /* eslint-disable */
 
-import '@polkadot/api/augment/rpc';
 import './augment-api-consts';
 import './augment-api-query';
 import './augment-api-rpc';
--- packages/types/src/interfaces/augment-api.ts
+++ packages/types/src/interfaces/augment-api.ts
@@ -1,7 +1,6 @@
 // Auto-generated , do not edit
 /* eslint-disable */
 
-import '@polkadot/api/augment/rpc';
 import './augment-api-consts';
 import './augment-api-query';
 import './augment-api-rpc';
